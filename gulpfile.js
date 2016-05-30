5525a419bfb44f5fcb1a2c1de503b3cc6ec3f6e3
Leo <todayliu@hotmail.com>
2016-05-30 14:07:03 +0800

    klass.js更名为index.js方便npm install安装

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ var uglify = require('gulp-uglify');
 var DEST = 'dist/';
 
 gulp.task('default', function() {
-  return gulp.src('klass.js')
+  return gulp.src('index.js')
     .pipe(gulp.dest(DEST))
     .pipe(uglify())
     .pipe(rename({ extname: '.min.js' }))
